7f446597fe54455d2e4bc0440c0b65a3f8c32351
gengfeng1 <gengliang.feng@163.com>
2024-12-29 16:56:30 +0800

    optimize shallow copy

--- springboot/src/java_basic/ShallowCopy/test.java
+++ springboot/src/java_basic/ShallowCopy/test.java
@@ -1,28 +1,27 @@
 package java_basic.ShallowCopy;
 
+import jdk.nashorn.internal.parser.JSONParser;
+
 public class test {
     public static void main(String[] args) throws CloneNotSupportedException {
         Employee e1 = new Employee("aaa", 12, new Address("aaaaa", 1111));
         System.out.println(e1);
         Employee e2 = e1;
-        System.out.println(e2);
-        e2.setId(11);
-        e2.setName("bbb");
-        e2.getAddress().setStreetId(112);
-        e2.getAddress().setStreetName("bbbbb");
+        Employee eClone = (Employee) e1.clone();
+        System.out.println(eClone);
+        eClone.setId(10);
+        eClone.setName("ccc");
+        eClone.getAddress().setStreetId(132);
+        eClone.getAddress().setStreetName("ccccc");
+        System.out.println("e1:");
         System.out.println(e1);
+        System.out.println(e1.address.streetId);
+        System.out.println("e2:");
+        System.out.println(e2.address.streetId);
         System.out.println(e2);
-
-
-        Employee e22 = (Employee) e1.clone();
-        System.out.println(e22);
-        e22.setId(10);
-        e22.setName("ccc");
-        e22.getAddress().setStreetId(132);
-        e22.getAddress().setStreetName("ccccc");
-        System.out.println(e1);
-        System.out.println(e2);
-        System.out.println(e22);
+        System.out.println("eClone:");
+        System.out.println(eClone.address.streetId);
+        System.out.println(eClone);
     }
 
 }
