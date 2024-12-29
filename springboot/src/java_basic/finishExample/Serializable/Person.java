2f13565039bae8bfd952f7c5d66c48a405ab3e99
gengfeng1 <gengliang.feng@163.com>
2024-12-29 20:34:05 +0800

    serialize

---
+++ springboot/src/java_basic/finishExample/Serializable/Person.java
@@ -0,0 +1,22 @@
+package java_basic.finishExample.Serializable;
+
+import java.io.Serializable;
+
+// 实现Serializable接口，表明该类可以被序列化
+class Person implements Serializable {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
